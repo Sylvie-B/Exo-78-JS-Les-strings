1ec4c7c23a528cd0c1bf78d7325f3c4ae25f6531
Sylvie-B <unsolo@hotmail.fr>
2020-12-14 09:56:16 +0100

    recup d'une chaine, de la longueur, des index

--- exercice1.js
+++ exercice1.js
@@ -2,15 +2,22 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
+let position = document.querySelector("span").innerHTML.indexOf('i');
+document.getElementById("position").innerHTML = position.toString();
 
+let chaine = document.querySelector('span').innerHTML;
+let majChaine = chaine.toUpperCase();
 
+document.getElementById("chaineMaj").innerHTML = majChaine;
 
-
-
-
-
+console.log(chaine);
+for (let i = 0 ; i < chaine.length ; i++){
+    if (chaine[i] === 'a'){
+        alert("lettre a trouvé" + i);
+    }
+}
 
